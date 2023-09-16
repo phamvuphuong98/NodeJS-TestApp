82b60b967aea5ad0ef9ea9b38ba90ce2bb9935ea
CHITANE <phamvuphuong98@gmail.com>
2023-09-16 20:22:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.send('Hello World! 1');
 });
 app.listen(3000, function () {
 	  console.log('Aplicación de pruebas escuchando en el puerto 3000!');
